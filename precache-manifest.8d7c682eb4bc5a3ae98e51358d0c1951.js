3a64341c3ca58fb2b184885b5c1bab5d11897b42
Map1en_ <maplenagisa@gmail.com>
2019-06-24 13:11:05 +0800

    Updates

---
+++ precache-manifest.8d7c682eb4bc5a3ae98e51358d0c1951.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c2f73d2f2a6224fb74b77c86493e108",
+    "url": "/15-Puzzle-React/index.html"
+  },
+  {
+    "revision": "aa3e5034a67c0503e5a3",
+    "url": "/15-Puzzle-React/static/css/main.3967eaff.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/15-Puzzle-React/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "aa3e5034a67c0503e5a3",
+    "url": "/15-Puzzle-React/static/js/main.6179db64.chunk.js"
+  },
+  {
+    "revision": "51c0bae6b4a5735d4036",
+    "url": "/15-Puzzle-React/static/js/runtime~main.c71b9a12.js"
+  }
+]);
